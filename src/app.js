19f5f356d7eaba2dfcdb0c3c3e8eb0f1052d86c9
ljimenezhi <ljimenezhi@vectoritcgroup.com>
2019-03-01 18:01:13 -0500

    Initial Commit

---
+++ src/app.js
@@ -0,0 +1,10 @@
+import { showAlert } from './messages';
+import $ from 'jquery';
+import uuid from 'uuid/v4'
+
+import  './styles.scss';
+
+document.getElementById('btn-alert')
+    .addEventListener('click', showAlert);
+
+$('#btn-jq').click(() => alert(uuid()))
